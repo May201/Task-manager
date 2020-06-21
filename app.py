5fee1491e457157d003e7e162b52465dd81af358
May201 <gs.malavika@yahoo.com>
2020-06-21 19:16:37 +0000

    Add requirements file and updated formatting for app.py

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello World..again'
 
+
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),debug=True)
+    app.run(
+        host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True
+    )
